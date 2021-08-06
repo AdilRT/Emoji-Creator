fe669b201121f3cb2ec2f5c1e73938835abec11b
AdilRT <46825650+AdilRT@users.noreply.github.com>
2021-08-06 13:11:27 +0545

    change train.py to train_CNN.py

---
+++ train_CNN.py
@@ -0,0 +1,61 @@
+import numpy as np
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os.path
+
+train_dir = 'data/train'
+val_dir = 'data/test'
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+validation_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+
+
+# cv2.ocl.setUseOpenCL(False)
+
+emotion_model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
+emotion_model_info = emotion_model.fit_generator(
+        train_generator,
+        steps_per_epoch=28709 // 64,
+        epochs=1,
+        validation_data=validation_generator,
+        validation_steps=7178 // 64)
+
+if os.path.isfile('models/emotion_model.h5') is False:
+        emotion_model.save('models/emotion_model.h5')
+
